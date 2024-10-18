ff1c0e3fac7a7cd29264d5da70f3e9b4da5da79a
jaykold <christianichebi@gmail.com>
2024-10-18 09:56:06 +0100

    Update

--- locators.py
+++ locators.py
@@ -12,8 +12,17 @@ driver.find_element(By.ID, "email").send_keys("test123@gmail.com")
 driver.find_element(By.ID, "password").send_keys("password")
 driver.find_element(By.ID, "confirmPassword").send_keys("password")
 time.sleep(2)
-driver.find_element(By.CLASS_NAME, "text-white").click()
 
+# //tagname[@attribute='value']
+submit_button = driver.find_element(By.XPATH, "//button[contains(@class, 'rounded-full')]")
+
+while submit_button.is_enabled() == False:
+    time.sleep(1)
+
+submit_button.click()
+
+
+time.sleep(2)
 # Assert the current URL
 expected_url = "https://quizzard-ten.vercel.app/"
 current_url = driver.current_url
